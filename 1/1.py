855c549665c5eeb0ff2a4af51651c3b7a212f8db
Florik3ks <floritrs@gmail.com>
2020-12-08 21:16:57 +0100

    first commit

---
+++ 1/1.py
@@ -0,0 +1,18 @@
+def solve(data):
+    for num in data:
+        for num2 in data:
+            if num != num2:
+                if int(num) + int(num2) == 2020:
+                    return int(num) * int(num2)
+
+def solve2(data):
+    for num in data:
+        for num2 in data:
+            for num3 in data:
+                if num != num2 and num != num3 and num2 != num3:
+                    if int(num) + int(num2) + int(num3) == 2020:
+                        return int(num) * int(num2) * int(num3)
+                        
+data = open("1/input.txt").read().split("\n")
+print(solve2(data))
+                    
---
+++ 4/4.py
@@ -0,0 +1,8 @@
+def solve(data):
+    return
+
+def solve2(data):
+    return
+
+data = open("4/input.txt").read().split("\n")
+print(solve2(data))
