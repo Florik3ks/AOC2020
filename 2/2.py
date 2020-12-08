d487cf8bceac8d88c76b7b2bf82922eac59fe432
Florik3ks <floritrs@gmail.com>
2020-12-08 22:33:21 +0100

    days 2 & 4

--- 2/2.py
+++ 2/2.py
@@ -1,8 +1,40 @@
 def solve(data):
-    return
+    valid = 0
+    for line in data:
+        character = line[line.index(":") - 1]
+        minimum = int(line.split("-")[0])
+        maximum = ""
+        for c in line.split("-")[1]:
+            maximum += c
+            if c == " ":
+                break
+        maximum = int(maximum)
+        count = 0
+        for char in line.split(":")[1]:
+            if char == character:
+                count += 1
+        if minimum <= count <= maximum:
+            valid += 1
+    return valid
+
 
 def solve2(data):
-    return
+    valid = 0
+    for line in data:
+        character = line[line.index(":") - 1]
+        index1 = int(line.split("-")[0]) - 1
+        index2 = ""
+        for c in line.split("-")[1]:
+            index2 += c
+            if c == " ":
+                break
+        index2 = int(index2) - 1
+        password = line.split(":")[1].strip()
+        if password[index1] == character and password[index2] != character or password[index1] != character and password[index2] == character:
+            valid += 1
+    return valid
+
 
 data = open("2/input.txt").read().split("\n")
+
 print(solve2(data))
--- 4/4.py
+++ 4/4.py
@@ -1,8 +1,81 @@
 def solve(data):
-    return
+    essential = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    valid = 0
+    for passport in data:
+        keys = []
+        values = []
+        for line in passport.split("\n"):
+            for kvPair in line.split():
+                keys.append(kvPair.split(":")[0])
+                values.append(kvPair.split(":")[1])
+        condition = True
+        for e in essential:
+            if e not in keys:
+                condition = False
+        if condition:
+            valid += 1
+
+    return valid
 
 def solve2(data):
-    return
+    essential = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    valid = 0
+    for passport in data:
+        keys = []
+        values = []
+        for line in passport.split("\n"):
+            for kvPair in line.split():
+                keys.append(kvPair.split(":")[0])
+                values.append(kvPair.split(":")[1])
+        
+        condition = True
+        for k in keys:
+            v = values[keys.index(k)]
+            if k == "byr":
+                if not 1920 <= int(v) <= 2002:
+                    condition = False
+            elif k == "iyr":
+                if not 2010 <= int(v) <= 2020:
+                    condition = False
+            elif k == "eyr":
+                if not 2020 <= int(v) <= 2030:
+                    condition = False
+            elif k == "hgt":
+                pre = v[len(v) - 2:]
+                post = v[:len(v) - 2]
+                if pre == "in":
+                    if not 59 <= int(post) <= 76:
+                        condition = False
+                elif pre == "cm":
+                    if not 150 <= int(post) <= 193:
+                        condition = False
+                else:
+                    condition = False
+            elif k == "hcl":
+                if len(v) == 7:
+                    if v[0] == "#":
+                        for char in v[1:]:
+                            if not char in "a b c d e f 0 1 2 3 4 5 6 7 8 9".split():
+                                condition = False
+                    else:
+                        condition = False
+                else:
+                    condition = False
+            elif k == "ecl":
+                if not v in ["amb","blu","brn","gry","grn","hzl","oth"]:
+                    condition = False
+            elif k == "pid":
+                if len(v) != 9 or not v.isnumeric():
+                    condition = False
 
-data = open("4/input.txt").read().split("\n")
+        for e in essential:
+            if e not in keys:
+                condition = False
+        if condition:
+            valid += 1
+
+    return valid
+
+
+data = open("4/input.txt").read().split("\n\n")
 print(solve2(data))
