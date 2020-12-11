8e218b6652d9d4abe1b61f1a80ebfdae532c3f4e
Florik3ks <floritrs@gmail.com>
2020-12-11 19:59:23 +0100

    8-1

--- 8/8.py
+++ 8/8.py
@@ -1,8 +1,24 @@
 def solve(data):
-    return
+    visitedLines = []
+    instruction = data[0]
+    instructionLine = 0
+    acc = 0
+    while instructionLine not in visitedLines:
+        instruction = data[instructionLine]
+        visitedLines.append(instructionLine)
+
+        instr = instruction.split()[0]
+        num = instruction.split()[1]
+        if instr == "acc":
+            acc += int(num)
+        elif instr == "jmp":
+            instructionLine = instructionLine + int(num) - 1
+        instructionLine += 1
+
+    return acc
 
 def solve2(data):
     return
 
 data = open("8/input.txt").read().split("\n")
-print(solve2(data))
+print(solve(data))
